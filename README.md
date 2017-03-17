ac280be43a3289dbc51209e02d6c997417e9211b
Pedro Marques <pedro.dalannar.marques@gmail.com>
2017-03-17 18:26:27 +0000

    Installed react components and webpack and setup js compilation. Added nodemon for instant server reinitialization and two new scripts to package.json. Updated README

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Repository for hexp web code i.e. api and user and admin frontends.
 2. `npm i` - install dependencies,
 3. `chmod +x pre-commit.sh && ./pre-commit.sh` - add pre-commit hook,
 4. `npm start` - server is listening on port 3000 (or whatever is specified with PORT env variable).
+
+## NPM commands
+1. `npm run nodemon` - Watch for changes in the server files (bin/www) and restart the server as the files are changed
+2. `npm run webpack` - Watch for changes in the javascript files (specified in the webpack.config.js) and compile them as they change
+3. `npm start` - Start the server normally
+4. `npm test` - Run the tests on the project
+5. `npm run lint` - Run the lint beautifier on the project files
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+	context: __dirname,
+	devtool: debug ? "inline-sourcemap" : null,
+
+	entry: {
+		app: path.join(__dirname, 'js', 'app', 'app')
+	},
+
+	module: {
+		loaders: [{
+			test: /\.js?$/,
+			exclude: /(node_modules|bower_components)/,
+			loader: 'babel-loader',
+			query: {
+				presets: ['react', 'es2015', 'stage-0'],
+				plugins: [],
+			}
+		}]
+	},
+
+	output: {
+		path: path.join(__dirname, "public", "scripts"),
+		filename: "[name].min.js"
+	},
+
+	plugins: debug ? [] : [
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+	]
+}
