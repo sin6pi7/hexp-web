59479ce64d0f59a0db47ca348910de0d5a379440
sin6pi7 <jac.zyla@gmail.com>
2017-06-01 12:06:54 +0100

    Import real movies.

--- scripts/db-populate.js
+++ scripts/db-populate.js
@@ -1,152 +1,71 @@
-const faker = require('faker');
 const Sequelize = require('sequelize');
-const { loadFixtures } = require('sequelize-fixtures');
-
 const createModels = require('../lib/api/models');
-const { VOTABLE_TYPES: { PERIOD, TAG, REGION } } = require('../lib/api/models/movie_votable');
+const MOVIES = [].concat(
+  require('../data/movies_base_data[2017-05-31-03-17-13].json'),
+  require('../data/movies_base_data[2017-06-01-11-29-51].json')
+);
 
 require('dotenv').load();
-
-// movies
-const MOVIE_COUNT = 100;
-const MOVIES = [];
-for (let i = 1; i <= MOVIE_COUNT; i++) {
-  MOVIES.push({
-    id: i,
-    model: 'movie',
-    data: {
-      title: faker.lorem.words(10),
-      description: faker.lorem.words(20),
-      release_date: faker.date.past(),
-      director: faker.name.findName(),
-      cast: `${faker.name.findName()}, ${faker.name.findName()}, ${faker.name.findName()}`,
-      country: faker.address.country(),
-      poster: faker.image.imageUrl(),
-      producer: faker.name.findName(),
-      trailer: faker.internet.url(),
-    },
-  });
-}
-// movie images
-const IMAGE_PER_MOVIE_COUNT = 5;
-const MOVIE_IMAGES = [];
-MOVIES.forEach((movie) => {
-  for (let i = 0; i < IMAGE_PER_MOVIE_COUNT; i++) {
-    MOVIE_IMAGES.push({
-      model: 'movie_image',
-      data: {
-        url: faker.image.imageUrl(
-          undefined,
-          undefined,
-          undefined,
-          true // randomize images
-        ),
-        movieId: movie.id,
-      },
-    });
-  }
-});
-// tags
-const TAG_COUNT = 50;
-const TAGS = [];
-for (let i = 0; i < TAG_COUNT; i++) {
-  const start = faker.random.number({
-    min: 0,
-    max: MOVIES.length - 1,
-  });
-  const end = faker.random.number({
-    min: start + 1,
-    max: MOVIES.length,
-  });
-  const movies = MOVIES
-    .slice(start, end)
-    .map(({ id }) => ({
-      id,
-      _through: {
-        score: faker.random.number(1000),
-        votableType: TAG,
-      }
-    }));
-
-  TAGS.push({
-    model: 'tag',
-    data: {
-      name: faker.random.word(),
-      movies,
-    },
-  });
-}
-// periods
-const PERIODS = require('../js/data/periods').map((name) => {
-  const start = faker.random.number({
-    min: 0,
-    max: MOVIES.length - 1,
-  });
-  const end = faker.random.number({
-    min: start + 1,
-    max: MOVIES.length,
-  });
-  const movies = MOVIES
-    .slice(start, end)
-    .map(({ id }) => ({
-      id,
-      _through: {
-        score: faker.random.number(1000),
-        votableType: PERIOD,
-      }
-    }));
-
-  return {
-    model: 'period',
-    data: {
-      name,
-      movies,
-    },
-  };
-});
-// regions
-const REGIONS = Object.keys(require('../js/data/regions')).map((code) => {
-  const start = faker.random.number({
-    min: 0,
-    max: MOVIES.length - 1,
-  });
-  const end = faker.random.number({
-    min: start + 1,
-    max: MOVIES.length,
-  });
-  const movies = MOVIES
-    .slice(start, end)
-    .map(({ id }) => ({
-      id,
-      _through: {
-        score: faker.random.number(1000),
-        votableType: REGION,
-      }
-    }));
-
-  return {
-    model: 'region',
-      data: {
-        code,
-        movies,
-      },
-  };
-});
-
 const db = new Sequelize(process.env.DATABASE_URI);
 const models = createModels(db);
 
-db.sync({ force: true })
+db
+  .sync({ force: true })
   .then(() => {
-    loadFixtures(
-      [].concat(
-        MOVIES,
-        MOVIE_IMAGES,
-        PERIODS,
-        REGIONS,
-        TAGS
-      ),
-      models
+    return Promise.all(
+      MOVIES.map(({
+        release_date,
+        title,
+        poster,
+        plot_desc, // description
+        epoch, // region
+        cc, // country codes
+        plot_desc_tags, // tags
+      }) => {
+        return models.movie.create({
+          title,
+          description: plot_desc,
+          release_date,
+          poster,
+        }).then((movie) => {
+          const addPeriodToMovie = (name, movie) => {
+            return models.period.findOrCreate({
+              where: {
+                name,
+              },
+            }).then(([period]) => movie.addPeriod(period));
+          };
+          const createAddRegionToMovie = (movie) => {
+            return (code) => {
+              return models.region.findOrCreate({
+                where: {
+                  code,
+                },
+              }).then(([region]) => movie.addRegion(region));
+            };
+          };
+          const createAddTagToMovie = (movie) => {
+            return (name) => {
+              return models.tag.findOrCreate({
+                where: {
+                  name,
+                },
+              }).then(([tag]) => movie.addTag(tag));
+            };
+          };
+          return Promise.all([
+            epoch
+              ? addPeriodToMovie(epoch, movie)
+              : Promise.resolve(),
+            cc && cc.length > 0
+              ? Promise.all(cc.map(createAddRegionToMovie(movie)))
+              : Promise.resolve(),
+            plot_desc_tags && plot_desc_tags.length > 0
+              ? Promise.all(plot_desc_tags.map(createAddTagToMovie(movie)))
+              : Promise.resolve(),
+          ]);
+        });
+      })
     );
   })
   .catch(console.error);
