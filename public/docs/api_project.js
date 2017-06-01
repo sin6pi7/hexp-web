23ea2b6082ca15056ca16397c9603c0f002a8229
sin6pi7 <jac.zyla@gmail.com>
2017-06-01 14:08:39 +0100

    Add API docs.

---
+++ public/docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "hexp-web",
+  "version": "1.0.0",
+  "description": "Hexp web: api, user and admin frontends.",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-06-01T13:06:44.016Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
